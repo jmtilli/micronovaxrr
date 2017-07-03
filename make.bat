9938b85214caefe148ea8a1d90bfb63f077dafc0
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-07-03 22:49:49 +0300

    Target Java 1.5

--- make.bat
+++ make.bat
@@ -3,6 +3,6 @@
 set CLASSPATH=.;.\deploy\jfreechart-1.0.1.jar;.\deploy\javafastcomplex.jar;.\deploy\Jama-1.0.3.jar
 
 del *.class
-javac *.java
+javac -Xlint:unchecked -source 1.5 -target 1.5 *.java
 jar cfm xrr.jar manifest.txt *.class *.png
 copy xrr.jar deploy
--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@
 export CLASSPATH=".:./deploy/jfreechart-1.0.1.jar:./deploy/javafastcomplex.jar:./deploy/Jama-1.0.3.jar"
 
 rm *.class
-javac *.java
+javac -Xlint:unchecked -source 1.5 -target 1.5 *.java
 jar cfm xrr.jar manifest.txt *.class *.png
 cp xrr.jar deploy
