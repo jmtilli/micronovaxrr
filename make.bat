9a34308960957b5fe88c52cfe9d29ff4a65d1dde
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-07-07 20:01:37 +0300

    Build improvements

--- make.bat
+++ make.bat
@@ -6,3 +6,12 @@ del *.class
 javac -Xlint:unchecked -source 1.5 -target 1.5 *.java
 jar cfm xrr.jar manifest.txt *.class *.png
 copy xrr.jar deploy
+copy javafastcomplex.jar deploy
+copy jfreechart-1.0.1.jar deploy
+copy Jama-1.0.3.jar deploy
+if not exist deploy\lib md deploy\lib
+copy lib\jcommon-1.0.0.jar deploy\lib
+copy atomic_masses.txt deploy
+copy default.layers deploy
+if not exist deploy\sf md deploy\sf
+copy sf\*.nff deploy\sf
--- make.sh
+++ make.sh
@@ -6,3 +6,12 @@ rm *.class
 javac -Xlint:unchecked -source 1.5 -target 1.5 *.java
 jar cfm xrr.jar manifest.txt *.class *.png
 cp xrr.jar deploy
+cp javafastcomplex.jar deploy
+cp jfreechart-1.0.1.jar deploy
+cp Jama-1.0.3.jar deploy
+mkdir -p deploy/lib
+cp lib/jcommon-1.0.0.jar deploy/lib
+cp atomic_masses.txt deploy
+cp default.layers deploy
+mkdir -p deploy/sf
+cp sf/*.nff deploy/sf
