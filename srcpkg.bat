ca210bb6263768bcb62a731da9b716a65102d76f
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-07-07 20:43:38 +0300

    Remove duplicates from repository and adjust build to work after it

--- srcpkg.bat
+++ srcpkg.bat
@@ -1,27 +1,39 @@
 @echo off
 mkdir xrr-src
-mkdir xrr-src\librarysrc
+mkdir xrr-src\lib
+mkdir xrr-src\sf
 mkdir xrr-src\deploy
-mkdir xrr-src\deploy\lib
-mkdir xrr-src\deploy\sf
 mkdir xrr-src\deploy\examples
-copy librarysrc\* xrr-src\librarysrc
-copy deploy\lib\* xrr-src\deploy\lib
-copy deploy\sf\* xrr-src\deploy\sf
+mkdir xrr-src\librarysrc
+
+copy lib\* xrr-src\lib
+
+copy sf\* xrr-src\sf
+
 copy deploy\examples\* xrr-src\deploy\examples
-copy deploy\README* xrr-src\deploy
+
 copy deploy\LICENSE.txt xrr-src\deploy
-copy deploy\jfreechart-1.0.1.jar xrr-src\deploy
-copy deploy\Jama-1.0.3.jar xrr-src\deploy
-copy deploy\javafastcomplex.jar xrr-src\deploy
-copy deploy\atomic_masses.txt xrr-src\deploy
+copy deploy\README* xrr-src\deploy
 copy deploy\resolution.txt xrr-src\deploy
+
+copy jfreechart-1.0.1.jar xrr-src
+copy Jama-1.0.3.jar xrr-src
+copy javafastcomplex.jar xrr-src
+copy atomic_masses.txt xrr-src
+
 copy deploy\run.sh xrr-src\deploy
 copy deploy\run.bat xrr-src\deploy
-copy deploy\default.layers xrr-src\deploy
+
+copy run.sh xrr-src
+copy run.bat xrr-src
+
+copy default.layers xrr-src
+
 copy *.java xrr-src
 copy *.png xrr-src
 copy *.sh xrr-src
 copy *.bat xrr-src
 copy manifest.txt xrr-src
+
+copy librarysrc\* xrr-src\librarysrc
 copy build.xml xrr-src
--- srcpkg.sh
+++ srcpkg.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 mkdir xrr-src
+mkdir xrr-src/lib
+mkdir xrr-src/sf
 mkdir xrr-src/deploy
-mkdir xrr-src/deploy/lib
-mkdir xrr-src/deploy/sf
 mkdir xrr-src/deploy/examples
 mkdir xrr-src/librarysrc
-cp deploy/lib/* xrr-src/deploy/lib
-cp deploy/sf/* xrr-src/deploy/sf
+cp lib/* xrr-src/lib
+cp sf/* xrr-src/sf
 cp deploy/examples/* xrr-src/deploy/examples
-cp deploy/LICENSE.txt deploy/README* deploy/resolution.txt deploy/jfreechart-1.0.1.jar deploy/Jama-1.0.3.jar deploy/javafastcomplex.jar deploy/atomic_masses.txt xrr-src/deploy
+cp deploy/LICENSE.txt deploy/README* deploy/resolution.txt xrr-src/deploy
+cp jfreechart-1.0.1.jar Jama-1.0.3.jar javafastcomplex.jar atomic_masses.txt xrr-src
 cp deploy/run.sh deploy/run.bat xrr-src/deploy
-cp deploy/default.layers xrr-src/deploy
+cp run.sh run.bat xrr-src
+cp default.layers xrr-src
 cp *.java *.png *.sh *.bat manifest.txt xrr-src
 cp librarysrc/* xrr-src/librarysrc
 cp build.xml xrr-src
