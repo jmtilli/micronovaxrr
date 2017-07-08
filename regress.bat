45987296421c773cc8a7cf0d5e9261ecd8b27981
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-07-08 10:50:51 +0300

    Remove old regression tests from suites

--- regress.bat
+++ regress.bat
@@ -1,8 +1,7 @@
 @echo off
+set CLASSPATH=.;.\deploy\jfreechart-1.0.1.jar;.\deploy\javafastcomplex.jar;.\deploy\Jama-1.0.3.jar
 echo Running regression tests...
 java -ea ChemicalFormula
 java -ea Fcode
-java -ea GraphData
-java -ea XRRSimul
 echo Regression tests completed
 pause
--- regress.sh
+++ regress.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
+export CLASSPATH=".:./deploy/jfreechart-1.0.1.jar:./deploy/javafastcomplex.jar:./deploy/Jama-1.0.3.jar"
 echo Running regression tests...
 java -ea ChemicalFormula
 java -ea Fcode
-java -ea GraphData
-java -ea XRRSimul
 echo Regression tests completed
